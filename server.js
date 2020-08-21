756580e22b4f4b573248fda123d825852b9b8915
Tohtamysh <tohtamysh@gmail.com>
2020-08-21 16:50:02 +0300

    add resize

--- server.js
+++ server.js
@@ -4,35 +4,18 @@ const multer = require('multer')
 
 const execa = require('execa');
 
+const sharp = require('sharp');
+
 const server = express()
 
 const storage = multer.memoryStorage()
 
 const upload = multer({ storage: storage })
 
-server.post('/', upload.any(), function(req, res) {
-    let file = req.files[0];
-
+function optimize(res, buffer, type) {
     const args = [];
 
-    if (file.mimetype == 'image/jpeg') {
-        args.push('-quality', '80');
-        args.push('-optimize');
-        args.push('-progressive');
-        args.push('-quant-table', '3');
-        args.push('-sample', '1x1');
-
-        (async() => {
-            const { stdout } = await execa('cjpeg', args, {
-                encoding: null,
-                input: file.buffer,
-                maxBuffer: Infinity
-            });
-            res.end(stdout);
-        })();
-    }
-
-    if (file.mimetype == 'image/png') {
+    if (type == 'image/png') {
         args.push('-o', '6');
         args.push('-i', '1');
         args.push('--strip', 'all');
@@ -45,11 +28,50 @@ server.post('/', upload.any(), function(req, res) {
         (async() => {
             const { stdout } = await execa('oxipng', args, {
                 encoding: null,
-                input: file.buffer,
+                input: buffer,
                 maxBuffer: Infinity
             });
             res.end(stdout);
         })();
+    }
+
+    if (type == 'image/jpeg') {
+        args.push('-quality', '80');
+        args.push('-optimize');
+        args.push('-progressive');
+        args.push('-quant-table', '3');
+        args.push('-sample', '1x1');
+
+        (async() => {
+            const { stdout } = await execa('cjpeg', args, {
+                encoding: null,
+                input: buffer,
+                maxBuffer: Infinity
+            });
+            res.end(stdout);
+        })();
+    }
+}
+
+server.post('/', upload.any(), function(req, res) {
+    let file = req.files[0];
+
+    const width = req.body.width ? parseInt(req.body.width) : null
+
+    const height = req.body.height ? parseInt(req.body.height) : null
+
+    if (width || height) {
+        sharp(file.buffer)
+            .resize({ width: width, height: height })
+            .toBuffer()
+            .then(data => {
+                optimize(res, data, file.mimetype)
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    } else {
+        optimize(res, file.buffer, file.mimetype)
     }
 });
 
